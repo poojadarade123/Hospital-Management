40a53ccb044c299699acac0c49790485d80d2955
Chetan-Chavan97 <chetanchavan1211.cc@gmail.com>
2022-04-04 21:16:42 +0530

    Chetan-Chavan97 | Admin update delete method created

--- Backend/Hospital-Management-System/src/main/java/com/springboot/app/controller/AdminController.java
+++ Backend/Hospital-Management-System/src/main/java/com/springboot/app/controller/AdminController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,5 +55,16 @@ public class AdminController {
 		return 1;
 	
 	    }
+	
+	@PutMapping("/editAdmin/{adminId}")
+	public int update(@PathVariable int adminId, @RequestBody Admin admin) {
+		return adminService.updateAdmin(adminId, admin);
+	}
+	
+	@DeleteMapping("/deleteAdmin/{adminId}")
+    public int delete(@PathVariable int adminId) { 		
+		return adminService.deleteAdmin(adminId);
+        
+    }  
 	
 }
